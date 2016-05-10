fc80864541e6792a0da904b487ca45fdd1104657
Maarten Bruggeman <maarten.bruggeman@appfoundry.be>
2016-05-10 13:27:50 +0200

    added support for default theming colors from Appcompat.

--- progressbutton/src/main/java/be/appfoundry/progressbutton/ProgressButton.java
+++ progressbutton/src/main/java/be/appfoundry/progressbutton/ProgressButton.java
@@ -32,6 +32,7 @@ import android.support.annotation.ColorInt;
 import android.support.annotation.IdRes;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -52,48 +53,90 @@ public class ProgressButton extends View {
     private static final int MAX_DEGREES = 360;
     private static final int MIN_SIZE = 48;
 
-    /** The background color of the button. */
+    /**
+     * The background color of the button.
+     */
     private int color;
-    /** The stroke color of the button. */
+    /**
+     * The stroke color of the button.
+     */
     private int strokeColor;
-    /** The color of the progress indicator. */
+    /**
+     * The color of the progress indicator.
+     */
     private int progressColor;
     private float strokeWidth;
-    /** The maximum progress. Defaults to 100. */
+    /**
+     * The maximum progress. Defaults to 100.
+     */
     private float maxProgress = DEFAULT_MAX_PROGRESS;
-    /** The value for each animation step. Defaults to 1 */
+    /**
+     * The value for each animation step. Defaults to 1
+     */
     private float animationStep = DEFAULT_ANIMATION_STEP;
-    /** Sets the button indeterminate or determinate. Defaults to true. */
+    /**
+     * Sets the button indeterminate or determinate. Defaults to true.
+     */
     private boolean indeterminate;
-    /** Sets the direction of the progress indicator. */
+    /**
+     * Sets the direction of the progress indicator.
+     */
     private boolean reverse = false;
-    /** The icon on the button. */
+    /**
+     * The icon on the button.
+     */
     private Drawable icon;
-    /** The radius of the outer circle. */
+    /**
+     * The radius of the outer circle.
+     */
     private float radius;
-    /** The current progress of the progress indicator. */
+    /**
+     * The current progress of the progress indicator.
+     */
     private float progress;
-    /** indicates if the indeterminate progress animation is running. Defaults to false. */
+    /**
+     * indicates if the indeterminate progress animation is running. Defaults to false.
+     */
     private boolean isAnimating = false;
-    /** The starting position for the progress indicator. */
+    /**
+     * The starting position for the progress indicator.
+     */
     private float startDegrees = DEFAULT_START_DEGREES;
-    /** The current starting point of the progress animation. */
+    /**
+     * The current starting point of the progress animation.
+     */
     private float startingPoint = DEFAULT_START_DEGREES;
-    /** Delay between animation frames. Defaults to 0. */
+    /**
+     * Delay between animation frames. Defaults to 0.
+     */
     private int animationDelay = DEFAULT_ANIMATION_DELAY;
-    /** The Paint for the inner circle. */
+    /**
+     * The Paint for the inner circle.
+     */
     private Paint circlePaint;
-    /** The Paint for the progress indicator. */
+    /**
+     * The Paint for the progress indicator.
+     */
     private Paint progressPaint;
-    /** The Paint for the outer circle. */
+    /**
+     * The Paint for the outer circle.
+     */
     private Paint strokePaint;
-    /** The amount of degrees for indicating the progress. */
+    /**
+     * The amount of degrees for indicating the progress.
+     */
     private float degrees;
-    /** The handler for the indeterminate progress animation */
+    /**
+     * The handler for the indeterminate progress animation
+     */
     private Handler animationHandler = new AnimationHandler(this);
-    /** The rectangle for drawing the button. */
+    /**
+     * The rectangle for drawing the button.
+     */
     RectF buttonRectF = new RectF();
-    /** The rectangle for drawing the icon. */
+    /**
+     * The rectangle for drawing the icon.
+     */
     Rect iconRect = new Rect();
 
 
@@ -122,24 +165,47 @@ public class ProgressButton extends View {
      * Initialise the {@link ProgressButton}.
      *
      * @param context the application environment
-     * @param attrs Attribute Set provided
+     * @param attrs   Attribute Set provided
      */
     private void init(Context context, AttributeSet attrs) {
         circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         progressPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         strokePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
+        Resources.Theme theme = context.getTheme();
+
         TypedArray attr = context.getTheme()
                 .obtainStyledAttributes(attrs, R.styleable.ProgressButton, 0, 0);
+        TypedValue value = new TypedValue();
+
+        int colorPrimary = 0xFFFFFFFF;
+        int colorPrimaryDark = 0x33000000;
+        int colorAccent = 0xFF000000;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            theme.resolveAttribute(android.R.attr.colorPrimary, value, true);
+            colorPrimary = value.data;
+            theme.resolveAttribute(android.R.attr.colorPrimaryDark, value, true);
+            colorPrimaryDark = value.data;
+            theme.resolveAttribute(android.R.attr.colorAccent, value, true);
+            colorAccent = value.data;
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            theme.resolveAttribute(android.support.v7.appcompat.R.attr.colorPrimary, value, true);
+            colorPrimary = value.data;
+            theme.resolveAttribute(android.support.v7.appcompat.R.attr.colorPrimaryDark, value, true);
+            colorPrimaryDark = value.data;
+            theme.resolveAttribute(android.support.v7.appcompat.R.attr.colorAccent, value, true);
+            colorAccent = value.data;
+        }
 
         try {
-            color = attr.getColor(R.styleable.ProgressButton_fillColor, 0xFFFFFFFF);
+            color = attr.getColor(R.styleable.ProgressButton_fillColor, colorPrimary);
             circlePaint.setColor(color);
-            strokeColor = attr.getColor(R.styleable.ProgressButton_strokeColor, 0xFFFFFFFF);
+            strokeColor = attr.getColor(R.styleable.ProgressButton_strokeColor, colorPrimaryDark);
             strokePaint.setColor(strokeColor);
-            progressColor = attr.getColor(R.styleable.ProgressButton_progressColor, 0xFFFFFFFF);
+            progressColor = attr.getColor(R.styleable.ProgressButton_progressColor, colorAccent);
             progressPaint.setColor(progressColor);
-            strokeWidth = attr.getDimension(R.styleable.ProgressButton_strokeWidth, 0f);
+            strokeWidth = attr.getDimension(R.styleable.ProgressButton_strokeWidth, 20f);
             indeterminate = attr.getBoolean(R.styleable.ProgressButton_indeterminate, true);
             icon = attr.getDrawable(R.styleable.ProgressButton_progressIcon);
         } finally {
@@ -147,33 +213,45 @@ public class ProgressButton extends View {
         }
     }
 
-    /** Returns true if the progress is indeterminate. */
+    /**
+     * Returns true if the progress is indeterminate.
+     */
     public boolean isIndeterminate() {
         return indeterminate;
     }
 
-    /** Sets if the progress is indeterminate or determinate. */
+    /**
+     * Sets if the progress is indeterminate or determinate.
+     */
     public void setIndeterminate(boolean indeterminate) {
         this.indeterminate = indeterminate;
         invalidate();
     }
 
-    /** Returns the maximum progress value of the indicator. */
+    /**
+     * Returns the maximum progress value of the indicator.
+     */
     public float getMaxProgress() {
         return maxProgress;
     }
 
-    /** Sets the maximus progress value of the indicator. */
+    /**
+     * Sets the maximus progress value of the indicator.
+     */
     public void setMaxProgress(float maxProgress) {
         this.maxProgress = maxProgress;
     }
 
-    /** Returns the current progress. */
+    /**
+     * Returns the current progress.
+     */
     public float getProgress() {
         return progress;
     }
 
-    /** Sets the current progress. (must be between 0 and maxProgress) */
+    /**
+     * Sets the current progress. (must be between 0 and maxProgress)
+     */
     public void setProgress(float progress) {
         if (progress > maxProgress || progress < 0) {
             throw new IllegalArgumentException(String.format(
@@ -189,102 +267,138 @@ public class ProgressButton extends View {
         invalidate();
     }
 
-    /** Returns the starting point of the progress indicator. */
+    /**
+     * Returns the starting point of the progress indicator.
+     */
     public float getStartDegrees() {
         return startDegrees;
     }
 
-    /** Sets the starting point of the progress indicator. */
+    /**
+     * Sets the starting point of the progress indicator.
+     */
     public void setStartDegrees(float degrees) {
         this.startDegrees = degrees;
         this.startingPoint = degrees;
         invalidate();
     }
 
-    /** returns the button icon. */
+    /**
+     * returns the button icon.
+     */
     public Drawable getIcon() {
         return icon;
     }
 
-    /** Sets the button icon. */
+    /**
+     * Sets the button icon.
+     */
     public void setIcon(@IdRes Drawable icon) {
         this.icon = icon;
         invalidate();
     }
 
-    /** Returns the background color of the button. */
+    /**
+     * Returns the background color of the button.
+     */
     public int getColor() {
         return color;
     }
 
-    /** Sets the background color of the button. */
+    /**
+     * Sets the background color of the button.
+     */
     public void setColor(@ColorInt int color) {
         this.color = color;
         circlePaint.setColor(color);
         invalidate();
     }
 
-    /** Returns the stroke color. */
+    /**
+     * Returns the stroke color.
+     */
     public int getStrokeColor() {
         return strokeColor;
     }
 
-    /** Sets the stroke color. */
+    /**
+     * Sets the stroke color.
+     */
     public void setStrokeColor(@ColorInt int strokeColor) {
         this.strokeColor = strokeColor;
         strokePaint.setColor(strokeColor);
         invalidate();
     }
 
-    /** Returns the color of the progress indicator. */
+    /**
+     * Returns the color of the progress indicator.
+     */
     public int getProgressColor() {
         return progressColor;
     }
 
-    /** Sets the color of the progress indicator. */
+    /**
+     * Sets the color of the progress indicator.
+     */
     public void setProgressColor(@ColorInt int progressColor) {
         this.progressColor = progressColor;
         progressPaint.setColor(progressColor);
         invalidate();
     }
 
-    /** Returns the value for each animation step. */
+    /**
+     * Returns the value for each animation step.
+     */
     public float getAnimationStep() {
         return animationStep;
     }
 
-    /** Sets the value for each animation step. */
+    /**
+     * Sets the value for each animation step.
+     */
     public void setAnimationStep(float animationStep) {
         this.animationStep = animationStep;
     }
 
-    /** Returns the animation delay. */
+    /**
+     * Returns the animation delay.
+     */
     public float getAnimationDelay() {
         return animationDelay;
     }
 
-    /** Sets the animation delay. */
+    /**
+     * Sets the animation delay.
+     */
     public void setAnimationDelay(int animationDelay) {
         this.animationDelay = animationDelay;
     }
 
-    /** Returns the radius of the button. */
+    /**
+     * Returns the radius of the button.
+     */
     public float getRadius() {
         return radius;
     }
 
-    /** Sets the radius of the button. */
+    /**
+     * Sets the radius of the button.
+     */
     public void setRadius(float radius) {
         this.radius = radius;
         invalidate();
     }
 
-    /** Returns the stroke width. */
+    /**
+     * Returns the stroke width.
+     */
     public float getStrokeWidth() {
         return strokeWidth;
     }
 
-    /** Sets the stroke width. */
+    /**
+     * Sets the stroke width.
+     */
     public void setStrokeWidth(float strokeWidth) {
         this.strokeWidth = strokeWidth;
         invalidate();
@@ -308,7 +422,9 @@ public class ProgressButton extends View {
         setMeasuredDimension(size, size);
     }
 
-    /** Calculate the size of the progressbutton */
+    /**
+     * Calculate the size of the progressbutton
+     */
     private int calculateSize(int resolvedWidth, int resolvedHeight, int width, int height) {
         int size;
 
@@ -346,7 +462,9 @@ public class ProgressButton extends View {
         return size;
     }
 
-    /** Calculate the default size of the progressbutton */
+    /**
+     * Calculate the default size of the progressbutton
+     */
     private int calculateDefaultSize(int resolvedWidth, int resolvedHeight, int width, int height) {
         int size;
         if (resolvedHeight == 0) {
@@ -368,7 +486,9 @@ public class ProgressButton extends View {
         return size;
     }
 
-    /** Convert dp value to pixels. */
+    /**
+     * Convert dp value to pixels.
+     */
     private static float convertDpToPixel(Context context, float dp) {
         Resources resources = context.getResources();
         DisplayMetrics metrics = resources.getDisplayMetrics();
@@ -412,7 +532,7 @@ public class ProgressButton extends View {
     /**
      * Sets the progress and starting point for the indeterminate progress animation.
      *
-     * @param progress Current progress value.
+     * @param progress     Current progress value.
      * @param startDegrees Current starting point.
      */
     private void setProgressStart(float progress, float startDegrees) {
@@ -422,7 +542,9 @@ public class ProgressButton extends View {
         invalidate();
     }
 
-    /** Starts the indeterminate progress animation. */
+    /**
+     * Starts the indeterminate progress animation.
+     */
     public void startAnimating() {
         if (indeterminate && !isAnimating) {
             isAnimating = true;
@@ -430,7 +552,9 @@ public class ProgressButton extends View {
         }
     }
 
-    /** Stops the indeterminate progress animation. */
+    /**
+     * Stops the indeterminate progress animation.
+     */
     public void stopAnimating() {
         if (isAnimating) {
             animationHandler.removeMessages(0);
@@ -443,7 +567,9 @@ public class ProgressButton extends View {
         }
     }
 
-    /** Handle the indeterminate progress animation */
+    /**
+     * Handle the indeterminate progress animation
+     */
     private void handleAnimation() {
         if (indeterminate) {
             if (progress >= maxProgress) {
@@ -469,7 +595,9 @@ public class ProgressButton extends View {
         }
     }
 
-    /** Handler class for handling the indeterminate progress animation */
+    /**
+     * Handler class for handling the indeterminate progress animation
+     */
     static class AnimationHandler extends Handler {
         private final WeakReference<ProgressButton> progressButtonReference;
 
